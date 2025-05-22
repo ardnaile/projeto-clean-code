e560a651be176365a9a1f5636faab6df7d8c8672
ardnaile <elimottacardoso@gmail.com>
2025-05-22 14:09:44 -0300

    refactor: remove comments from mappers

--- src/main/java/projetowebquiz/backend/mappers/QuestaoQuatroMapper.java
+++ src/main/java/projetowebquiz/backend/mappers/QuestaoQuatroMapper.java
@@ -7,8 +7,6 @@ import projetowebquiz.backend.models.QuestaoQuatro;
 @Component
 public class QuestaoQuatroMapper {
 
-  // quando inicializa o dto por meio da conversão, as opcoes são nulas
-
   public QuestaoQuatroDto toDto(QuestaoQuatro questaoQuatro) {
     return new QuestaoQuatroDto(
         questaoQuatro.getEnunciado(),
